dafa505348302fc3c0d5530cbcc7a811a5278efa
ubuntu16.04 <yangyz07@vanke.com>
2020-05-14 19:22:05 -0700

    use freq

--- instant_noodles.py
+++ instant_noodles.py
@@ -9,30 +9,49 @@ import PIL.Image as Image
 # from matplotlib import pyplot as plt
 from wordcloud import WordCloud
 
+remove_words_map = {
+    "noodles": ["方便面", "拉面", "面皮", "面条", "阿宽", "康师傅", "汤达人", "吃", "味道", "泡面",
+                "外观", "包装", "袋子", "超级", "非常", "凉皮", "袋装", "点评", "面饼",
+                "这", "没", "的", '是', u'也', u'都', u'就是', u'在', u'了',
+                "\[[\w\s]+\]"]  # 最后是去掉表情包
+}
 
-def words_freq_plot(content):
+
+def words_freq_plot(input_content):
     # 文本分词
-    cut_txt = jieba.cut(content, cut_all=False)
+    cut_txt = jieba.cut(input_content, cut_all=False)
     object_list = []
-    remove_words = ["方便面", "拉面", "面条", "康师傅", "汤达人", "吃", "味道", "泡面", "这", "没", "的", '是', u'也', u'都', u'就是', u'在', u'了']
 
     # 词频统计
     for word in cut_txt:
-        if word not in remove_words:
-            object_list.append(word)
+        object_list.append(word)
 
     word_counts = collections.Counter(object_list)
 
     wd = WordCloud(
         font_path=font_path,  # 设置字体格式，不然会乱码
         background_color="white",  # 设置背景颜色
-        mask=background_image  # 设置背景图
+        mask=background_image,  # 设置背景图
+        max_words=100  # 最多展示100个词
     ).generate_from_frequencies(word_counts)
     return wd
 
 
-def analysis_plot(content):
-    tags = jieba.analyse.extract_tags(content, topK=50, withWeight=False)
+def analysis_plot(input_content):
+    tags = jieba.analyse.extract_tags(input_content, topK=100, withWeight=False)
+    text = " ".join(tags)
+    wd = WordCloud(
+        font_path=font_path,  # 设置字体格式，不然会乱码
+        background_color="white",  # 设置背景颜色
+        mask=background_image  # 设置背景图
+    ).generate(text)
+    return wd
+
+
+def combine(input_content):
+    cut_txt = jieba.cut(input_content, cut_all=False)
+    input_content = " ".join(cut_txt)
+    tags = jieba.analyse.extract_tags(input_content, topK=100, withWeight=False)
     text = " ".join(tags)
     wd = WordCloud(
         font_path=font_path,  # 设置字体格式，不然会乱码
@@ -87,24 +106,25 @@ for spuid in spuid_list:
     title_list.append([review.get("title", "") for review in review_list])
     content_list.append([review.get("content", "") for review in review_list])
 
-for i in range(10):
 
+for i in range(10):
     # 定义词频背景
     background_image = np.array(Image.open("duck_image.jpg"))
     font_path = os.path.join(os.path.dirname(__file__), "SIMSUN.TTC")
     # 文本预处理
     content = rec_reason_list[i]+"".join(title_list[i])+"".join(content_list[i])
-    remove_words = ["方便面", "拉面", "面皮", "面条", "阿宽", "康师傅", "汤达人", "吃", "味道", "泡面",
-                    "这", "没", "的", '是', u'也', u'都', u'就是', u'在', u'了',
-                    "\[[\w\s]+\]"]
+    remove_words = remove_words_map["noodles"]
     content = re.sub("|".join(remove_words), '', content)
     content = re.sub(r'[^\w\s]', '', content)
     wd1 = words_freq_plot(content)
-    wd2 = analysis_plot(content)
+    # wd2 = analysis_plot(content)
+    # wd3 = combine(content)
 
     # 保存词云图
     wd1.to_file('pics/rank_%s_freq.png' % i)
-    wd2.to_file('pics/rank_%s_ana.png' % i)
+    # wd2.to_file('pics/rank_%s_ana.png' % i)
+    # wd3.to_file('pics/rank_%s_com.png' % i)
+
     # 显示词云图
     # plt.imshow(wd, interpolation="bilinear")
     # plt.axis("off")
